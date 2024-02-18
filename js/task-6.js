a674ecc9d692460eb7494caab11f75c9a58b3b17
sashok1987nik <sashok1987nk@ukr.net>
2024-02-18 11:50:08 +0100

    up

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputRef = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = Number(inputRef.value);
+
+  if (amount < 1 || amount > 100 || !Number.isInteger(amount)) {
+    return alert('Please enter a number between 1 and 100');
+  }
+
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+
+  boxesRef.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesRef.innerHTML = '';
+}
+
